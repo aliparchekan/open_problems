6431b93b80482d68e91a9e7df56043423f7731f3
aliparchekan <aliparchekan@gmail.com>
2022-08-30 11:27:48 -0400

    Time: 359 ms (24.57%), Space: 15.5 MB (65.18%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,9 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for num in nums:
-            if num == 0:
-                nums.remove(num)
-                nums.append(num)
+        lastNonZero = 0
+        for current in range(len(nums)):
+            if nums[current] != 0:
+                nums[lastNonZero] = nums[current]
+                lastNonZero += 1
+        
+        for current in range(lastNonZero, len(nums)):
+            nums[current] = 0
             
+            
+                
         
