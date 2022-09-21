a7d251c95f69d18af3e31b743e9dd478a31e337c
aliparchekan <aliparchekan@gmail.com>
2022-09-20 23:35:12 -0400

    Time: 36 ms (19.90%), Space: 5.8 MB (90.92%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if (0 < x && x % 10 == 0) return false;
+        int half = 0;
+        while(x > half){
+            half = half * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == half || x == half / 10;
+    }
+};
